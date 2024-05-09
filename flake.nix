c44ca59634fc385545cd77995dd3236a9f243c95
LarsZauberer <wasser.ian@gmail.com>
2024-05-09 15:36:50 +0200

    Full nix build config

--- flake.nix
+++ flake.nix
@@ -3,24 +3,47 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
-      pname = "rustapple";
-      version = "0.1";
-      cargoLock.lockFile = ./Cargo.lock;
-      src = pkgs.lib.cleanSource ./.;
-      nativeBuildInputs = [
-        pkgs.pkg-config
-        pkgs.alsa-lib
-      ];
-    };
-  };
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
+      in {
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "rustapple";
+            version = "0.1";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.alsa-lib
+            ];
+            buildInputs = [
+              pkgs.pkg-config
+              pkgs.alsa-lib
+            ];
+          };
+        };
+
+        # Expose executable
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/rustapple";
+        };
+
+        defaultPackage = self.packages.${system}.default;
+      }
+    );
 }
